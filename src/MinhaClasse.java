7a4647da47e01046c48ddce730e4e4c72ed1b2b7
nilton menezes <niltonmenezes@live.com>
2025-02-03 10:08:10 -0300

    treinando criação de variáveis

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,11 @@
 public class MinhaClasse {
     public static void main (String [] args) {
-        System.out.print("Aprendendo Java!");
+        String myName = "Nilt";
+
+        int year = 2024;
+
+        boolean yesOrNot = true;
+
+        year = 2025;
     }
 }
