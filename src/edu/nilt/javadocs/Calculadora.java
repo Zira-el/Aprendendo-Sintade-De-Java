914a826aa24d3e2a335f23a02cb53acd729de25b
nilton menezes <niltonmenezes@live.com>
2025-02-04 17:23:58 -0300

    criando documentações

---
+++ src/edu/nilt/javadocs/Calculadora.java
@@ -0,0 +1,23 @@
+package edu.nilt.javadocs;
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor.
+ *
+ * @author Nilt Menezes
+ * @version 1.0.0
+ * @since 04/02/2025
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois esté é o segundo parâmetro do método
+     * @return int o resultado desde método é a soma dos dois números.
+     */
+    public int somar (int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/edu/nilt/javadocs/Comentarios.java
@@ -0,0 +1,18 @@
+package edu.nilt.javadocs;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, sou um comentário de uma linha.
+
+        /* Olá,
+         * Eu sou um comentário que posso ser mais detalhado
+         * quando necessário.
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você pretende elaborar
+     * um comentário a nível de documentação.
+     */
+    public void metodo(String[] args) {}
+}
