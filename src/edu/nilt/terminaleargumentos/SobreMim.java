27964b5e2b4652a32e4a2767cc14f932009b2864
nilton menezes <niltonmenezes@live.com>
2025-02-04 18:22:03 -0300

    usando scanner para fazer uma perguntar e esperar uma resposta pelo terminal

--- src/edu/nilt/terminaleargumentos/SobreMim.java
+++ src/edu/nilt/terminaleargumentos/SobreMim.java
@@ -1,11 +1,23 @@
 package edu.nilt.terminaleargumentos;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class SobreMim {
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.parseInt(args[2]);
-        double altura = Double.parseDouble(args[3]);
+        // Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura:");
+        double altura = scanner.nextDouble();
 
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos.");
