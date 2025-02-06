00cfd281c67e3ee31f30cc18edb5939d184a9b7e
nilton menezes <niltonmenezes@live.com>
2025-02-06 11:07:09 -0300

    adicionando validações simples ao about me

--- src/edu/nilt/terminaleargumentos/SobreMim.java
+++ src/edu/nilt/terminaleargumentos/SobreMim.java
@@ -1,26 +1,32 @@
 package edu.nilt.terminaleargumentos;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class SobreMim {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // Criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            System.out.println("Digite o seu nome:");
+            String nome = scanner.next();
 
-        System.out.println("Digite o seu nome:");
-        String nome = scanner.next();
+            System.out.println("Digite o seu sobrenome:");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite o seu sobrenome:");
-        String sobrenome = scanner.next();
+            System.out.println("Digite a sua idade:");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite a sua idade:");
-        int idade = scanner.nextInt();
+            System.out.println("Digite a sua altura:");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Digite a sua altura:");
-        double altura = scanner.nextDouble();
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos.");
+            System.out.println("Minha altura é " + altura + "cm.");
+        } catch ( InputMismatchException error ) {
+            System.err.println("Os campos idade e altura precisam ser numéricos e as casas decimais devem ser separadas por ponto.");
+        }
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos.");
-        System.out.println("Minha altura é " + altura + "cm.");
+        scanner.close();
     }
 }
